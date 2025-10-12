60fb3318355eef763ddcf3b7e91c60201be3f19a
jackhexiangyu12 <87466111+jackhexiangyu12@users.noreply.github.com>
2025-10-12 21:19:06 +0800

    xixi update

--- static/js/index.js
+++ static/js/index.js
@@ -5,17 +5,30 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		loop: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
+	// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 	
     bulmaSlider.attach();
+
+    // Tabs functionality for Build Your Security AI Agent section
+    $('.build-tabs li').on('click', function() {
+        var tabId = $(this).data('tab');
+        
+        // Remove active class from all tabs and panes
+        $('.build-tabs li').removeClass('is-active');
+        $('.tab-pane').removeClass('is-active');
+        
+        // Add active class to clicked tab and corresponding pane
+        $(this).addClass('is-active');
+        $('#' + tabId).addClass('is-active');
+    });
 
 })
